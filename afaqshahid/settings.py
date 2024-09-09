3a363bfd43b7fc8a5d100d85a7a55432f1b28e4a
afaqonline <afaqshahid@aol.com>
2024-09-09 22:38:23 +0500

    Portfolio Updates

--- afaqshahid/settings.py
+++ afaqshahid/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-)cvv!+nlj!lshd8g46oy(=t@@uvr5t(@0810=3^+*si-f3_dfo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+#ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 # Application definition
 
